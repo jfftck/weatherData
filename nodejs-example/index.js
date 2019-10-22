302e4393448d18b31307f95f63997daaa7867687
Jeffrey Tackett <jeffrey@pop-os.localdomain>
2019-10-21 18:46:10 -0700

    Initial Commit

---
+++ nodejs-example/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+/**
+ * Return basic server info and available data.
+ */
+app.get('/', (req, res) => res.send({
+  server: {
+    name: 'API Starter Server',
+    apiVersion: '0.2'
+  },
+  availableDataSeries: {
+    increasingData: {
+      name: 'Increasing data values',
+      description: 'Numbers 1 to 5'
+    },
+    flatData: {
+      name: 'Flat data values',
+      description: 'Just the number 5'
+    }
+  }
+}))
+
+/**
+ * Hard code some linear data.
+ */
+app.get('/api/increasingData', (req, res) => res.send({
+  format: 'date',
+  initialDataSet: [
+    [Date.now(), 1],
+    [Date.now() + 10, 2],
+    [Date.now() + 20, 3],
+    [Date.now() + 30, 4],
+    [Date.now() + 40, 5],
+  ]
+}))
+
+/**
+ * Hard code some flat data to simply give variety.
+ */
+app.get('/api/flatData', (req, res) => res.send({
+  format: 'date',
+  initialDataSet: [
+    [Date.now(), 5],
+    [Date.now() + 10, 5],
+    [Date.now() + 20, 5],
+    [Date.now() + 30, 5],
+    [Date.now() + 40, 5],
+  ]
+}))
+
+app.listen(3050, () => console.log('Listening on port 3050!'))
